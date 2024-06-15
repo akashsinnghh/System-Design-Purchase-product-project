1b9e152d1b78334f235f9d7f1948b13ed948fae1
akashsinnghh <akash.singh1200@gmail.com>
2024-06-15 19:19:25 +0530

    customer side api done

--- coustomer/src/database/models/Customer.js
+++ coustomer/src/database/models/Customer.js
@@ -12,17 +12,34 @@ const CustomerSchema = new Schema({
     ],
     cart: [
         {
-          product: { type: Schema.Types.ObjectId, ref: 'product', require: true},
+          product: {
+            _id: { type: String, required: true},
+            name: { type: String },
+            banner: { type: String },
+            price: { type: Number }
+          },
           unit: { type: Number, require: true}
         }
     ],
     wishlist:[
         { 
-            type: Schema.Types.ObjectId, ref: 'product', require: true
+            product: {
+                _id: { type: String, required: true},
+                name: { type: String },
+                description: { type: String },
+                banner: { type: String },
+                available: { type: Boolean },
+                price: { type: Number }
+              },
+              unit: { type: Number, require: true}
         }
     ],
     orders: [ 
-        { type: Schema.Types.ObjectId, ref: 'order', require: true }
+        {
+            _id: { type: String, required: true},
+            amount: { type: String, required: true},
+            date: { type: Date, default: Date.now() },
+        }
     ]
 },{
     toJSON: {
